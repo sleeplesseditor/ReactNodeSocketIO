b5cc3950c565f319213d06b05a3c765aa403a90f
sleeplesseditor <josh@bryson.me.uk>
2018-07-20 16:02:41 +0100

    Route error fixed

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send({ response: 'I am alive'}).status(200);
+    res.send({ response: 'I am alive' }).status(200);
 });
+
+module.exports = router;
